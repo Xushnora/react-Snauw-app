107376c0c5c7c7ea759e0003dbf298ca203a7ca0
Xushnora <xushnoranishonova@gmail.com>
2022-05-09 01:13:13 -0700

    changed className

--- src/AllBlocks/Footer/footer.js
+++ src/AllBlocks/Footer/footer.js
@@ -6,93 +6,93 @@ import logoFooter from "../../imgs/logo-sinauw.svg"
 function Footer() {
     return (
         <footer id="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="footer-block">
-                        <a class="link-logo" href="#">
+            <div className="container">
+                <div className="row">
+                    <div className="footer-block">
+                        <a className="link-logo" href="#">
                             <img src={logoFooter} alt="logo" style={{width: "117", height: "56"}} />
                         </a>
-                        <div class="texts">
-                            <p class="footer-text">Any feedback or questions?
+                        <div className="texts">
+                            <p className="footer-text">Any feedback or questions?
                             </p>
-                            <p class="footer-text after">Contact us on our social media</p>
+                            <p className="footer-text after">Contact us on our social media</p>
                         </div>
-                        <ul class="social-list row middle">
-                            <li class="social-item list-sty">
+                        <ul className="social-list row middle">
+                            <li className="social-item list-sty">
                                 <a href="#" target="_blank">
-                                    <i class='bx bxl-facebook'></i>
+                                    <i className='bx bxl-facebook'></i>
                                 </a>
                             </li>
-                            <li class="social-item list-sty">
+                            <li className="social-item list-sty">
                                 <a href="#" target="_blank">
-                                    <i class='bx bxl-twitter' ></i>
+                                    <i className='bx bxl-twitter' ></i>
                                 </a>
                             </li>
-                            <li class="social-item list-sty">
+                            <li className="social-item list-sty">
                                 <a href="#" target="_blank">
-                                    <i class='bx bxl-instagram'></i>
+                                    <i className='bx bxl-instagram'></i>
                                 </a>
                             </li>
                         </ul>
                     </div>
-                    <ul class="footer-list row between"> 
-                        <li class="footer-item list-sty">
-                            <h3 class="footer-title">Menu</h3>
-                            <ul class="in-list">
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Revisions</a>
+                    <ul className="footer-list row between"> 
+                        <li className="footer-item list-sty">
+                            <h3 className="footer-title">Menu</h3>
+                            <ul className="in-list">
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Revisions</a>
                                 </li>
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Sheets</a>
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Sheets</a>
                                 </li>
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Quiz</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="footer-item list-sty">
-                            <h3 class="footer-title">Services</h3>
-                            <ul class="in-list">
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">About us</a>
-                                </li>
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Partners</a>
-                                </li>
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Contact us</a>
-                                </li>
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Careers</a>
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Quiz</a>
                                 </li>
                             </ul>
                         </li>
-                        <li class="footer-item list-sty">
-                            <h3 class="footer-title">Helpful link</h3>
-                            <ul class="in-list">
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Conditions</a>
+                        <li className="footer-item list-sty">
+                            <h3 className="footer-title">Services</h3>
+                            <ul className="in-list">
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">About us</a>
                                 </li>
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Solutions</a>
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Partners</a>
+                                </li>
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Contact us</a>
+                                </li>
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Careers</a>
                                 </li>
                             </ul>
                         </li>
-                        <li class="footer-item list-sty">
-                            <h3 class="footer-title">Apps Download</h3>
-                            <ul class="in-list">
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Google Play</a>
+                        <li className="footer-item list-sty">
+                            <h3 className="footer-title">Helpful link</h3>
+                            <ul className="in-list">
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Conditions</a>
                                 </li>
-                                <li class="in-item list-sty">
-                                    <a class="fo-item-link text-none" href="#">Solutions</a>
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Solutions</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className="footer-item list-sty">
+                            <h3 className="footer-title">Apps Download</h3>
+                            <ul className="in-list">
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Google Play</a>
+                                </li>
+                                <li className="in-item list-sty">
+                                    <a className="fo-item-link text-none" href="#">Solutions</a>
                                 </li>
                             </ul>
                         </li>
                     </ul>
                 </div>
-                <div class="finally-link-div">
-                    <a class="finally-link" href="#">Copyright © 2021 Sinauw. All Rights Reserved.</a>
+                <div className="finally-link-div">
+                    <a className="finally-link" href="#">Copyright © 2021 Sinauw. All Rights Reserved.</a>
                 </div>
             </div>
         </footer>
