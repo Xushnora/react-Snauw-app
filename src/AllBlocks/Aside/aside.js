6c3af0f6880ebec359e87cf85f00266f76434f4b
Xushnora <xushnoranishonova@gmail.com>
2022-05-09 01:24:48 -0700

    changed clasname

--- src/AllBlocks/Aside/aside.js
+++ src/AllBlocks/Aside/aside.js
@@ -8,18 +8,18 @@ import phoneImg from "../../imgs/Group 19.svg"
 function Aside() {
     return (
         <section>
-            <div class="container">
-                <div class="apps-portion row middle around">
-                    <div class="apps-desc">
-                        <h2 class="apps-title">Get Our Apps, Now!</h2>
-                        <p class="apps-text">Reach out our apps everywhere and everytime with Sinauw Mobile Application.</p>
-                        <div class="row middle">
-                            <img class="gg-ap" src={googleImg} style={{width: "164", height: "72"}} alt="google" />
+            <div className="container">
+                <div className="apps-portion row middle around">
+                    <div className="apps-desc">
+                        <h2 className="apps-title">Get Our Apps, Now!</h2>
+                        <p className="apps-text">Reach out our apps everywhere and everytime with Sinauw Mobile Application.</p>
+                        <div className="row middle">
+                            <img className="gg-ap" src={googleImg} style={{width: "164", height: "72"}} alt="google" />
                             <img src={appStoryImg} alt="app-story" style={{width: "147", height: "50"}} />
                         </div>
                     </div>
-                    <div class="apps-img">
-                        <img class="phone" src={phoneImg} alt="phone" />
+                    <div className="apps-img">
+                        <img className="phone" src={phoneImg} alt="phone" />
                     </div>
                 </div>
             </div>
